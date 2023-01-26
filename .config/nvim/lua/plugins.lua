4d8af1827ae90a7aa51bcaa43ca88f639c5f5dbf
NeutralAlice <53350098+NeutralAlice@users.noreply.github.com>
2023-01-26 23:16:34 +0000

    adding more config

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,18 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- You add plugins here 
+  use 'tpope/vim-fugitive'
+  use {
+  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+-- or                            , branch = '0.1.x',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+  use {
+  'lewis6991/gitsigns.nvim',
+  -- tag = 'release' -- To use the latest release (do not use this if you run Neovim nightly or dev builds!)
+}
+end)
